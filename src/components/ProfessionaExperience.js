69f75a5865a3b40d778a1b3cd512371c84696f0f
fedora <rodolfo.dechiara@outlook.it>
2021-05-13 19:15:56 +0200

    feat: style professional experience form

--- src/components/ProfessionaExperience.js
+++ src/components/ProfessionaExperience.js
@@ -1,32 +1,39 @@
 import React from 'react';
-
+import './ProfessionalExperience.css'
 class ProfessionalExperience extends React.Component
 {
     constructor(props)
     {
         super(props);
+        this.state =
+        {
+            compiled: false
+        };
     }
 
     render()
     {
         return (
+            
             <form>
-                <fieldset>
-                    <legend>Esperienze professionali</legend>
-                    <label htmlFor="posizione">Occupazione o posizione ricoperta</label>
-                    <input type="text" placeholder="Titolo dell\'occupazione" name="posizione" required></input>
-                    <label htmlFor="datore">Datore di lavoro</label>
-                    <input type="text" placeholder="" name="datore" required></input>
-                    <label htmlFor="città">Città</label>
-                    <input type="text" placeholder="Luogo di lavoro" name="città" required></input>
-                    <label htmlFor="paese">Paese</label>
-                    <input type="text" placeholder="Nazione di lavoro" name="paese" required></input>
-                    <label htmlFor="inizio">Data di inizio</label>
-                    <input type="date" name="inizio" required></input>
-                    <label htmlFor="fine">Data di fine</label>
-                    <input type="date" name="fine" required></input>
-                    <button>{`Avanti >`}</button>
-                </fieldset>
+                <h3>Nuova esperienza professionale</h3>
+                <label htmlFor="posizione">Occupazione o posizione ricoperta</label>
+                <input type="text" placeholder="Titolo dell'occupazione" name="posizione" required></input>
+                <label htmlFor="datore">Datore di lavoro</label>
+                <input type="text" placeholder="" name="datore" required></input>
+                <label htmlFor="città">Città</label>
+                <input type="text" placeholder="Luogo di lavoro" name="città" required></input>
+                <label htmlFor="paese">Paese</label>
+                <input type="text" placeholder="Nazione di lavoro" name="paese" required></input>
+                <label htmlFor="inizio">Data di inizio</label>
+                <input type="date" name="inizio" required></input>
+                <label htmlFor="fine">Data di fine</label>
+                <input type="date" name="fine" required></input>
+                <label htmlFor="mansioni">Principali attività e responsabilità</label>
+                <textarea placeholder="Descrivi i tuoi compiti e responsabilità" rows="5"></textarea>
+                <div onClick={this.props.handleClick}> <strong>+</strong> aggiungi altre esperienze lavorative</div>
+                <button onClick={this.props.handleClick}>{`Avanti >`}</button>
+             
             </form>
         );
     }
