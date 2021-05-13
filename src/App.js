3e6032c19e7b74321b0fa098a3c7185724027186
fedora <rodolfo.dechiara@outlook.it>
2021-05-13 18:49:13 +0200

    test: professional experience rendering

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import React from 'react';
-import {PersonalDataForm} from './components/PersonaDataForm';
+//import {PersonalDataForm} from './components/PersonaDataForm';
+import ProfessionalExperience from './components/ProfessionaExperience';
 function App() {
   return (
-    <PersonalDataForm/>
+    <ProfessionalExperience/>
   );
 }
 
