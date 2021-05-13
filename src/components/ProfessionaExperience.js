371269e19af682cc3ea6e22eff2f188d9cf5713c
fedora <rodolfo.dechiara@outlook.it>
2021-05-13 18:46:11 +0200

    feat: add form to add past professional experience

--- src/components/ProfessionaExperience.js
+++ src/components/ProfessionaExperience.js
@@ -13,8 +13,23 @@ class ProfessionalExperience extends React.Component
             <form>
                 <fieldset>
                     <legend>Esperienze professionali</legend>
+                    <label htmlFor="posizione">Occupazione o posizione ricoperta</label>
+                    <input type="text" placeholder="Titolo dell\'occupazione" name="posizione" required></input>
+                    <label htmlFor="datore">Datore di lavoro</label>
+                    <input type="text" placeholder="" name="datore" required></input>
+                    <label htmlFor="città">Città</label>
+                    <input type="text" placeholder="Luogo di lavoro" name="città" required></input>
+                    <label htmlFor="paese">Paese</label>
+                    <input type="text" placeholder="Nazione di lavoro" name="paese" required></input>
+                    <label htmlFor="inizio">Data di inizio</label>
+                    <input type="date" name="inizio" required></input>
+                    <label htmlFor="fine">Data di fine</label>
+                    <input type="date" name="fine" required></input>
+                    <button>{`Avanti >`}</button>
                 </fieldset>
             </form>
         );
     }
 }
+
+export default ProfessionalExperience;
